d2622a82bd3fff75dde9d7f7277e8e24fcd6ea4f
IkuTsujihiro <63435523+IkuTsujihiro@users.noreply.github.com>
2020-04-25 00:08:52 +0900

    Added functionality for the personality quiz on flask

--- personality_quiz_webapp.py
+++ personality_quiz_webapp.py
@@ -1,18 +1,54 @@
 from flask import Flask, render_template, request
+import personality_quiz
+
 app = Flask(__name__)
+
+global state
+state = {
+    "question":"None",
+    "answers": [],
+    "score": [0, 0],
+    "count":0
+}
 
 @app.route('/')
 @app.route('/home')
-def main():
+def home():
     return render_template("home.html")
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
-@app.route('/quiz')
+@app.route('/quiz', methods=['GET', "POST"])
 def quiz():
-    return render_template('quiz.html')
+    global state
+    if request.method == 'GET':
+        question = personality_quiz.questions[state['count']]
+        state['question'] = question['question']
+        state['answers'] = [ans['answer'] for ans in question['answers']]
+        return render_template('quiz.html', state=state)
+    elif request.method == 'POST':
+        response = request.form[str(state['count'])]
+        question = personality_quiz.questions[state['count']]
+        answer = [a for a in question['answers'] if a['answer'] == response]
+        score = answer[0]['value']
+        orientation = answer[0]['orientation']
+
+        if orientation == "x":
+            state['score'][0] += score
+        elif orientation == "y":
+            state['score'][1] += score
+        
+        state['count'] += 1
+
+        if state['count'] == 2:
+            return render_template('results.html', state=state)
+        else:
+            question = personality_quiz.questions[state['count']]
+            state['question'] = question['question']
+            state['answers'] = [ans['answer'] for ans in question['answers']]
+            return render_template('quiz.html', state=state)
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=3000)
